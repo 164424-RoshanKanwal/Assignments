0743769f4a0c31cf2691222c32c837b641190039
164424-RoshanKanwal <44638941+164424-RoshanKanwal@users.noreply.github.com>
2019-01-17 18:19:38 +0530

    17 Jan 2019

---
+++ courseAPI2-1/src/main/java/com/roshan/springbootcricbuzz/CourseApi21Application.java
@@ -0,0 +1,14 @@
+package com.roshan.springbootcricbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApi21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApi21Application.class, args);
+	}
+
+}
+
---
+++ courseAPI2-1/src/main/java/com/roshan/springbootcricbuzz/controller/BinaryDataController.java
@@ -0,0 +1,55 @@
+package com.roshan.springbootcricbuzz.controller;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsOperations;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.gridfs.GridFSDBFile;
+
+@RestController
+public class BinaryDataController {
+
+	@Autowired
+	private GridFsOperations gridFsOperations;
+	
+	String fieldId="";
+	
+	 @RequestMapping("/")
+	    public String home(){
+	        return "Hello World!";
+	    }
+	 
+	 @RequestMapping("/saveImage")
+	public String saveFile() throws FileNotFoundException {
+
+		DBObject metadata = new BasicDBObject();
+		metadata.put("organization", "Roshan Kanwal");
+		InputStream inputStream = new FileInputStream("src/main/resources/Images/two.png");  //C:\Users\rkanwal\Desktop\1.jpeg
+		metadata.put("type","image");
+		fieldId= gridFsOperations.store(inputStream,"two.png", "image/png", metadata).getId().toString();
+		System.out.println("FieldId = " + fieldId);
+		
+		return "Successfully added";
+	}
+
+	 @RequestMapping("/retrieve/image")
+	public String retrieveFile() throws IOException {
+		 
+		GridFSDBFile dbFile =  gridFsOperations.findOne(new Query(Criteria.where("_id").is(fieldId)));
+		
+		System.out.println(dbFile.getInputStream());
+		System.out.println("FieldId = " + dbFile.getFilename());
+		return "File Retrieved";
+	}
+}
